269bf1d83864f29b2fc00f1d401b65a3abc90496
Gor Martsen <gor.martsen@gmail.com>
2018-04-13 23:36:19 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # vue-directive-primer-tooltip
-VueJS directive to use primer tooltip
+VueJS directive to use GitHub primer tooltip (css only based)
+Very light and performance oriented.
+
+### Usage
+
+```js
+var Tooltip = require('vue-directive-primer-tooltip')
+Vue.use(Tooltip)
+```
+
+```html
+<span v-primer-tooltip.ne.no-delay="'my text'">some text</span>
+```
+
+```scss
+@import "vue-directive-primer-tooltip/index.scss"
+```
+
+More details about GitHub Primer is [here](https://primer.github.io/archive/tooltips/).
+
+### License
+
+The code is available under the [MIT](LICENSE) license.
+
+### Contributing
+
+We are open to contributions
---
+++ index.js
@@ -0,0 +1,71 @@
+module.exports = {
+  name: 'tooltip',
+  config: {},
+  install: function install(Vue) {
+    Vue.directive('primer-tooltip', {
+      inserted: function inserted(el, binding) {
+        processModifiers(binding, el);
+      },
+      updated: function componentUpdated(el, binding) {
+        cleanClasses(el)
+        processModifiers(binding, el);
+      } 
+    });
+  }
+}
+
+function processModifiers(_ref, el) {
+  if (typeof _ref.value === 'string') {
+    el.setAttribute('aria-label', _ref.value);
+  }
+  for (var modifier in _ref.modifiers) {
+    setClasses(modifier, el);
+  }
+  if (_ref.argument) {
+    setClasses(_ref.argument, el);
+  }
+  ensureClass('tooltipped', el);
+}
+
+function setClasses(name, el){
+  var names = [
+    'n',
+    'ne',
+    'e',
+    'se',
+    's',
+    'sw',
+    'w',
+    'nw',
+    'no-delay',
+  ]
+  if (names.indexOf(name) !== -1) {
+    ensureClass('tooltipped-' + name, el);
+  }
+}
+
+function ensureClass(item, el) {
+  if (!el.classList.contains(item)) {
+    el.classList.add(item)
+  }
+}
+
+function cleanClasses(el) {
+  var classes = [
+    "tooltipped",
+    "tooltipped-n",
+    "tooltipped-ne",
+    "tooltipped-e",
+    "tooltipped-se",
+    "tooltipped-s",
+    "tooltipped-sw",
+    "tooltipped-w",
+    "tooltipped-nw",
+    "tooltipped-no-delay"
+  ]
+  classes.forEach(function(item){
+    if (el.classList.contains(item)) {
+      el.classList.remove(item)
+    }  
+  });
+}
